839f66e09398451a920fab5e02ab74a449e0b1d0
Henrique Borelli <henriqborelli225@yahoo.com>
2026-04-27 21:15:01 -0300

    Add files via upload

---
+++ Objetos JS/Objeto 1/script.js
@@ -0,0 +1,40 @@
+let carro = {
+    nome: "HRV",
+    marca: "Honda",
+    modelo:"SUV",
+    Qualidades:["Design moderno e atraente", "Espaço interno generoso", "Economia de combustível","Segurança confiável"]
+}
+console.log(carro)
+console.log(carro["nome"])
+console.log(carro.marca)
+console.log(carro.modelo)
+console.log(carro.qualidades)
+
+let corinthians = {
+    nome: "Sport club corinthians paulista",
+    fundação:"1910",
+    estadio: "Neo quimica arena",
+    conquistas:[ "1 libertadores", "2 mundiais", "31 Paulistoes","1 recopa sula", "7 brasileiroes", "4 copas do brasil", "2 Super copas do brasil", "pai-do-palmeiras"]
+}
+
+console.log(corinthians)
+console.log(corinthians["nome"])
+console.log(corinthians.fundação)
+console.log(corinthians.estadio)
+console.log(corinthians.conquistas)
+
+let Bleach = {
+    Protagonista: "Ichigo Kurosaki",
+    bankai: "Tensa zangetsu",
+    Deutragonista: "Rukia kuchiki",
+    Antagonistas:["Yhwach","Sosuke Aizen","Ulquiorra cifer", "Grimmjow","Stark"],
+    capitoes:["Renji abarai"," genrunsai Yamamoto","Byakuya kuchiki, shunsui","kenpachi zaraki"," unohana, gin ishimori", "Toshiro hitsugaya"],
+    mundos: ["Soul society","Terra", "hueco mundo"],
+}
+console.log(Bleach)
+console.log(Bleach.Protagonista)
+console.log(Bleach.bankai)
+console.log(Bleach.Deutragonista)
+console.log(Bleach.Antagonistas)
+console.log(Bleach.capitoes)
+console.log(Bleach.mundos)
